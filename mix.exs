4d67c2245c3dc37d058b9f348ee65319af86b20e
Paweł Świątkowski <katafrakt@users.noreply.github.com>
2024-11-13 21:23:48 +0100

    Add CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Fey.MixProject do
     [
       app: :fey,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
